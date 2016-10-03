50c7b7f8d65b680a50975ca927abd12b7bd3021a
samiam21 <samiam21@gmail.com>
2016-10-03 11:58:23 -0500

    Initial commit of VS project

---
+++ KeyboardWalker/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace KeyboardWalker
+{
+    class Program
+    {
+        public static string[,] keyboardLayout = new string[6, 6]
+        {
+            {"A", "B", "C", "D", "E", "F" },
+            {"G", "H", "I", "J", "K", "L" },
+            {"M", "N", "O", "P", "Q", "R" },
+            {"S", "T", "U", "V", "W", "X" },
+            {"Y", "Z", "1", "2", "3", "4" },
+            {"5", "6", "7", "8", "9", "0" }
+        };
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
