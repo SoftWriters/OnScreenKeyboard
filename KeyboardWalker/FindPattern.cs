451837bece5f2106a06feba68265ad5903ff0123
samiam21 <samiam21@gmail.com>
2016-10-03 13:11:12 -0500

    Added function to find the letter on the keyboard

--- KeyboardWalker/FindPattern.cs
+++ KeyboardWalker/FindPattern.cs
@@ -1,24 +1,95 @@
-﻿namespace KeyboardWalker
+﻿using System;
+
+namespace KeyboardWalker
 {
     public class FindPattern
     {
-        private string[,] _keyboardLayout = new string[6, 6]
+        #region Public Structs
+        public struct Coordinate
         {
-            {"A", "B", "C", "D", "E", "F" },            // 65 - 70
-            {"G", "H", "I", "J", "K", "L" },            // 71 - 76
-            {"M", "N", "O", "P", "Q", "R" },            // 77 - 82
-            {"S", "T", "U", "V", "W", "X" },            // 83 - 88
-            {"Y", "Z", "1", "2", "3", "4" },            // 89 - 90, 49 - 52
-            {"5", "6", "7", "8", "9", "0" }             // 53 - 57, 48
-        };
+            public int? X;
+            public int? Y;
 
-        public FindPattern(string[,] keyboardLayout = null)
+            public Coordinate(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+
+            public override bool Equals(object obj)
+            {
+                bool ret = false;
+
+                if (obj is Coordinate)
+                {
+                    Coordinate c = (Coordinate)obj;
+                    ret = ((X == c.X) && (Y == c.Y));
+                }
+
+                return ret;
+            }
+        }
+        #endregion
+
+        #region Private Class Members
+        private char[][] _keyboardLayout = new char[6][]
+        {
+            new char[6] {'A', 'B', 'C', 'D', 'E', 'F' },
+            new char[6] {'G', 'H', 'I', 'J', 'K', 'L' },
+            new char[6] {'M', 'N', 'O', 'P', 'Q', 'R' },
+            new char[6] {'S', 'T', 'U', 'V', 'W', 'X' },
+            new char[6] {'Y', 'Z', '1', '2', '3', '4' },
+            new char[6] {'5', '6', '7', '8', '9', '0' }
+        };
+        #endregion
+
+        #region Getters & Setters
+        public char[][] GetKeyboardLayout()
+        {
+            return _keyboardLayout;
+        }
+
+        public void SetKeyboardLayout(char[][] keyboardLayout)
+        {
+            _keyboardLayout = keyboardLayout;
+        }
+        #endregion
+
+        #region Constructor
+        public FindPattern(char[][] keyboardLayout = null)
         {
             // Check to see if a new keyboard layout was given
-            if (keyboardLayout == null)
+            if (keyboardLayout != null)
             {
                 _keyboardLayout = keyboardLayout;
             }
         }
+        #endregion
+
+        #region Public Action Methods
+
+        #endregion
+
+        #region Public Helpers
+        public Coordinate FindKey(char key)
+        {
+            Coordinate ret = new Coordinate();
+
+            // Loop through each row of the keyboard
+            for (int x = 0; x < _keyboardLayout.Length; x++)
+            {
+                // Check for the specified character in the keyboard
+                int pos = Array.IndexOf(_keyboardLayout[x], key);
+                if (pos > -1)
+                {
+                    // Found the item, set the coordinate and stop looking
+                    ret.X = x;
+                    ret.Y = pos;
+                }
+            }
+
+            return ret;
+        }
+        #endregion
     }
 }
--- KeyboardWriterUT/UnitTests.cs
+++ KeyboardWriterUT/UnitTests.cs
@@ -1,5 +1,6 @@
 ﻿using KeyboardWalker;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static KeyboardWalker.FindPattern;
 
 namespace KeyboardWriterUT
 {
@@ -11,20 +12,65 @@ namespace KeyboardWriterUT
         {
             // Create default object and check valid
             FindPattern obj = new FindPattern();
-            Assert.IsNotNull(obj, "Failed to create default FindPattern object");
+            Assert.IsNotNull(obj.GetKeyboardLayout(), "Failed to create default FindPattern object");
 
             // Create object with custom keyboard and check valid
-            string[,] keyboardLayout = new string[6, 6]
+            char[][] keyboardLayout = new char[5][]
             {
-                {"A", "B", "C", "D", "E", "F" },
-                {"G", "H", "I", "J", "K", "L" },
-                {"M", "N", "O", "P", "Q", "R" },
-                {"S", "T", "U", "V", "W", "X" },
-                {"Y", "Z", "0", "1", "2", "3" },
-                {"4", "5", "6", "7", "8", "9" }
+                new char[6] {'A', 'B', 'C', 'D', 'E', 'F' },
+                new char[6] {'G', 'H', 'I', 'J', 'K', 'L' },
+                new char[6] {'M', 'N', 'O', 'P', 'Q', 'R' },
+                new char[6] {'S', 'T', 'U', 'V', 'W', 'X' },
+                new char[6] {'Y', 'Z', '0', '1', '2', '3' }
             };
             obj = new FindPattern(keyboardLayout);
-            Assert.IsNotNull(obj, "Failed to create FindPattern object with custom keyboard layout");
+            Assert.IsNotNull(obj.GetKeyboardLayout(), "Failed to create FindPattern object with custom keyboard layout");
+        }
+
+        [TestMethod]
+        public void UT_FindKey()
+        {
+            // Create the object for testing (default keyboard)
+            FindPattern fp = new FindPattern();
+            Coordinate expected, test;
+
+            // Find the first letter
+            expected = new Coordinate(0, 0);
+            test = fp.FindKey('A');
+            Assert.AreEqual(expected, test, "Failed to find the first character on the keyboard");
+
+            // Find the last letter
+            expected = new Coordinate(5, 5);
+            test = fp.FindKey('0');
+            Assert.AreEqual(expected, test, "Failed to find the last character on the keyboard");
+
+            // Find some middle letters
+            expected = new Coordinate(3, 1);
+            test = fp.FindKey('T');
+            Assert.AreEqual(expected, test, "Failed to find T on the keyboard");
+
+            expected = new Coordinate(5, 0);
+            test = fp.FindKey('5');
+            Assert.AreEqual(expected, test, "Failed to find 5 on the keyboard");
+
+            // Check non-existant character
+            expected = new Coordinate();
+            test = fp.FindKey('#');
+            Assert.AreEqual(expected, test, "Failed to return emtpy coordinate on non-existant character");
+
+            // Check with non-default keyboard
+            char[][] keyboardLayout = new char[5][]
+            {
+                new char[6] {'A', 'B', 'C', 'D', 'E', 'F' },
+                new char[6] {'G', 'H', 'I', 'J', 'K', 'L' },
+                new char[6] {'M', 'N', 'O', 'P', 'Q', 'R' },
+                new char[6] {'S', 'T', 'U', 'V', 'W', 'X' },
+                new char[6] {'Y', 'Z', '0', '1', '2', '3' }
+            };
+            fp = new FindPattern(keyboardLayout);
+            expected = new Coordinate(4, 5);
+            test = fp.FindKey('3');
+            Assert.AreEqual(expected, test, "Failed to find last key on non-default keyboard");
         }
     }
 }
