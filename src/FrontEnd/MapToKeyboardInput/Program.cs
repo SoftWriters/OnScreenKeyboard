ca5bc8daa52f1f3063cc36baef1a69d7b1c5e166
lukeatron <1340234+lukeatron@users.noreply.github.com>
2018-01-09 10:10:07 -0500

    Added command line tool to run the translation.

--- src/FrontEnd/MapToKeyboardInput/Program.cs
+++ src/FrontEnd/MapToKeyboardInput/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace MapToKeyboardInput
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var stdin = Console.OpenStandardInput())
+            using (var stdout = Console.OpenStandardOutput())
+                await ProcessAsync(stdin, stdout);
+        }
+
+        /// <summary>
+        /// Read lines from the input stream and write the cursor instructions to the output stream.
+        /// </summary>
+        private static async Task ProcessAsync(Stream stdin, Stream stdout)
+        {
+            var processor = new OnScreenKeyboard.Logic.InputProcessor(new OnScreenKeyboard.Logic.SixBySixKeyboardLayout());
+
+            using (var reader = new StreamReader(stdin))
+            using (var writer = new StreamWriter(stdout))
+            {
+                string input = null;
+                while ((input = await reader.ReadLineAsync()) != null)
+                    writer.WriteLine(processor.ToCursorInstructions(input));
+            }
+
         }
     }
 }
