02c89033783fdde9103cd2ef0673c1dcc36d782c
Justin Kramer <jpk91@pitt.edu>
2019-10-01 19:41:51 -0400

    Methods establishes for handling of converted input lines to DVR keyboard

--- code/OnScreenKeyboardApplication/OnScreenKeyboardApplication/KeyboardConverter.cs
+++ code/OnScreenKeyboardApplication/OnScreenKeyboardApplication/KeyboardConverter.cs
@@ -8,11 +8,18 @@ namespace OnScreenKeyboardApplication
         {
             string[] splitInputLines = inputLines.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
 
+            string convertedLines = "";
+
             foreach (string inputLine in splitInputLines)
             {
-
+                convertedLines += ChangeToKeyboardMovements(inputLine) + "\n";
             }
-            return splitInputLines[0];
+            return convertedLines;
+        }
+
+        private string ChangeToKeyboardMovements(string inputLine)
+        {
+            return inputLine;
         }
     }
 }
