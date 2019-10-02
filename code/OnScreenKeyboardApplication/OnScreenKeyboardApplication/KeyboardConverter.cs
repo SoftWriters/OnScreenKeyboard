ece423943c5e4ef7a06a3bf38d180828fc1498b6
Justin Kramer <jpk91@pitt.edu>
2019-10-01 20:16:38 -0400

    Setup for keyboard mapping methods, just need implementations

--- code/OnScreenKeyboardApplication/OnScreenKeyboardApplication/KeyboardConverter.cs
+++ code/OnScreenKeyboardApplication/OnScreenKeyboardApplication/KeyboardConverter.cs
@@ -4,6 +4,9 @@ namespace OnScreenKeyboardApplication
 {
     class KeyboardConverter
     {
+        private string keyboardMovementsLine;
+        private KeyboardMapping currentMapping;
+
         public string ConvertLines(string inputLines)
         {
             string[] splitInputLines = inputLines.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
@@ -19,7 +22,76 @@ namespace OnScreenKeyboardApplication
 
         private string ChangeToKeyboardMovements(string inputLine)
         {
-            return inputLine;
+            keyboardMovementsLine = "";
+
+            currentMapping = new KeyboardMapping();
+
+            foreach (char inputCharacter in inputLine)
+            {
+                if (currentMapping.SpaceCharacter(inputCharacter))
+                {
+                    keyboardMovementsLine += "S" + ",";
+                }
+                else
+                {
+                    AddKeyboardPositionMovements(inputCharacter);
+                }
+
+                AddSelectCharacter();
+            }
+
+            RemoveFinalComma();
+
+            return keyboardMovementsLine;
+        }
+
+        private void RemoveFinalComma()
+        {
+            keyboardMovementsLine.Remove(keyboardMovementsLine.Length - 1);
+        }
+
+        private void AddKeyboardPositionMovements(char inputCharacter)
+        {
+            int verticalMoves = currentMapping.FindVerticalMovement(inputCharacter);
+            int horizontalMoves = currentMapping.FindHorizontalMovement(inputCharacter);
+
+            AddVerticalMovement(verticalMoves);
+            AddHorizontalMovement(horizontalMoves);
+        }
+
+        private void AddVerticalMovement(int verticalMoves)
+        {
+            for (int i = 0; i < Math.Abs(verticalMoves); i++)
+            {
+                if (verticalMoves < 0)
+                {
+                    keyboardMovementsLine += "U" + ",";
+                }
+                else
+                {
+                    keyboardMovementsLine += "D" + ",";
+                }
+            }
+        }
+
+        private void AddHorizontalMovement(int horizontalMoves)
+        {
+            for (int i = 0; i < Math.Abs(horizontalMoves); i++)
+            {
+                if (horizontalMoves < 0)
+                {
+                    keyboardMovementsLine += "L" + ",";
+                }
+                else
+                {
+                    keyboardMovementsLine += "R" + ",";
+                }
+            }
+        }
+
+        private void AddSelectCharacter()
+        {
+            keyboardMovementsLine += "#" + ",";
         }
     }
 }
---
+++ code/OnScreenKeyboardApplication/OnScreenKeyboardApplication/KeyboardMapping.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnScreenKeyboardApplication
+{
+    class KeyboardMapping
+    {
+        private int currentXPosition;
+        private int currentYPosition;
+
+        public KeyboardMapping()
+        {
+            currentXPosition = 0;
+            currentYPosition = 0;
+        }
+
+        public bool SpaceCharacter(char inputCharacter)
+        {
+            // Implement
+            return false;
+        }
+
+        public int FindVerticalMovement(char inputCharacter)
+        {
+            return 0;
+        }
+
+        public int FindHorizontalMovement(char inputCharacter)
+        {
+            return 0;
+        }
+    }
+}
