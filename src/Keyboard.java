0d29eaa147cc060511d834f75f8c80b589c1e9c0
michaelbarton <mjb7480@rit.edu>
2019-10-23 16:29:39 -0400

    Some initial design planning/brainstorming

--- src/Keyboard.java
+++ src/Keyboard.java
@@ -1,2 +1,28 @@
-public abstract class Keyboard {
+/**
+ * We use an abstract class for Keyboard for future possibilities of different keyboard layouts
+ * that a user wants to script input for
+ */
+public class Keyboard {
+
+    /**
+     * The keyboard mapping to use for scripting the lines from input files
+     */
+    private char[][] keyboardLayout;
+
+    /**
+     * Constructor for this class, sets value of keyboard
+     * @param keyboardLayout - the keyboard mapping to be used for this instance of Keyboard
+     */
+    public Keyboard(char[][] keyboardLayout) {
+        this.keyboardLayout = keyboardLayout;
+    }
+
+    /**
+     * Takes the input and determines the script necessary to obtain that input
+     * @param input - a String to convert into a script
+     * @return a String with the script representing how to obtain the input for this particular keyboard
+     */
+    public String scriptLine(String input) {
+        return input;
+    }
 }
---
+++ src/KeyboardScripter.java
@@ -0,0 +1,11 @@
+/**
+ * The driver class for this program, takes file input, reads it
+ * and outputs scripted version of each line
+ */
+public class KeyboardScripter {
+
+    public static void main(String[] args) {
+
+    }
+
+}
